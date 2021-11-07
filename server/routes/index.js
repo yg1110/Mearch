4cd972a276f3fc5e5cd4ae81e235a0edd55c18ab
yg1110 <younggil9488@gmail.com>
2021-11-08 00:11:44 +0900

    크롤링 추가 및 스케줄러 구현

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,60 @@
 const express = require('express')
+const cheerio = require('cheerio')
+const axios = require('axios')
+const schedule = require('node-schedule')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json('root')
+const info = {
+  title: [],
+  sale: [],
+  cash: [],
+  img: [],
+}
+
+const getDocument = async () => {
+  try {
+    return await axios.get(
+      'https://store.musinsa.com/app/clearance/lists?clearance_year=&clearance_month=&clearance_week=&d_cat_cd=&brand=&sale_fr_rate=&sale_to_rate=&sort=pop_category&sub_sort=&page=1&list_kind=small&ex_soldout=&exclusive_yn=&price=&price1=&price2=',
+    )
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const crawling = () => {
+  return getDocument().then(document => {
+    const $ = cheerio.load(document.data)
+
+    const $li_box = $('.li_box').toArray()
+    $li_box.forEach(element => {
+      const el = cheerio.load(element)
+      const title = el('.list_info').text().trim()
+      const src = el('.lazy').attr('data-original')
+      const img = `https:${src}`
+      const price = el('.price').find('del').text()
+      const salePrice = el('.price').text().split(price)[1].trim()
+      const sale = el('.icon_new').text()
+      info.img.push(img)
+      info.sale.push(sale)
+      info.title.push(title)
+      info.cash.push({price: price, salePrice: salePrice})
+    })
+
+    return info
+  })
+}
+
+schedule.scheduleJob('0 11 * * * *', () => {
+  crawling().then(info => {
+    console.log(info)
+  })
 })
 
-router.get('/api', (req, res) => {
-  res.json('api')
+router.get('/', (req, res) => {
+  crawling().then(info => {
+    res.send(info)
+  })
 })
 
 module.exports = router
